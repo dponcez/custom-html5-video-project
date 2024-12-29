30519fd1af97f2c73419be5dd70c546cf560dfa8
Damian Ponce <dponcez_git@hotmail.com>
2024-12-29 18:19:07 -0500

    feat: update the video player structure

--- modules/template.js
+++ modules/template.js
@@ -1,7 +1,6 @@
 import { selector } from "./custom_functions.js";
 
 export const template = () => {
-
   const main = selector('.main');
   
   main.innerHTML = `
@@ -14,60 +13,61 @@ export const template = () => {
         <button class="toggle inactive" id="dark-mode" aria-describedby="darkMode" role="button" data-dark-mode></button>
       </div>
     </header>
-    <figure class="figure--container">
-      <div class="player--container">
-        <div class="hidden" data-player="player" role="main">
-          <div class="video--container" id="container">
-            <video class="video" src="./assets/video/Chosen.mp4" poster="./assets/images/poster.jpg" data-source></video>
+    <figure class="video--player">
+      <div class="hidden">
+        <div class="video--container">
+          <video data-source poster="../assets/images/poster.jpg"></video>
+        </div>
+        <div class="control--container flex" id="controlContainer" aria-describedby="controlContainer" role="complementary">
+          <div class="progress--bar" id="progressBar" aria-describedby="progressBar" role="progressbar" data-progress-bar>
+            <div class="progress" id="progress" aria-describedby="progress" role="progressbar" data-progress></div>
           </div>
-          <section class="controls" role="controls" aria-describedby="controlContainer">
-            <div class="progress" role="progressbar" data-progress>
-              <div class="progress--bar" data-progress-bar></div>
-              <div class="progress__slider" data-progress-slider></div>
-            </div>
-            <div class="timer--container" role="timer container">
-              <span class="timer" data-timer="timer" role="timer">0:00/0:00</span>
-            </div>
-            <div class="controllers" role="controlContainer">
-              <div class="player--controls">
-                <button class="prev btn btn--medium" aria-describedby="previousBbutton" data-prev>
-                    <i class="fas fa-chevron-left"></i>
-                </button>
-                <button class="play btn btn--large" aria-describedby="playButton" data-play>
-                    <i class="fas fa-play"></i>
-                </button>
-                <button class="next btn btn--medium" aria-describedby="nextButton" data-next>
-                    <i class="fas fa-chevron-right"></i>
-                </button>
-              </div>
-            </div>
-            <div class="skip--container" role="skip buttons">
-              <button class="rewind--btn btn flex" id="rewindButton" aria-describedby="rewindButton" role="button" data-skip="-10">
-                -10
-                <i class="fa fa-solid fa-backward"></i>
+          <div class="flex">
+            <span class="current--time timer inactive" aria-current="time" role="timer" data-current-time>00:00</span>
+            <span>/</span>
+            <span class="duration timer inactive" aria-current="time" role="timer" data-duration>00:00</span>
+          </div>
+          <div class="controls flex" aria-describedby="controls" role="complementary">
+            <button class="rewind--btn btn flex" id="rewind" aria-describedby="rewind" role="button" data-skip="-10">
+              -10
+              <i class="fa fa-solid fa-backward"></i>
+            </button>
+            <div class="player--container flex">
+              <button class="chevron--left btn flex" id="chevron-left" aria-describedby="chevronLeft" role="button" data-backward>
+                <i class="fa-solid fa-chevron-left"></i>
               </button>
-              <button class="forward--btn btn flex" id="forwardButton" aria-describedby="forwardButton" role="button" data-skip="+25">
-                <i class="fa-solid fa-forward"></i>
-                +25
+              <button class="play--btn btn flex" id="playButton" aria-describedby="playButton" role="button" data-play>
+                <i class="fa-solid fa-play"></i>
+              </button>
+              <button class="chevron--right btn flex" id="chevron-right" aria-describedby="chevronRight" role="button" data-forward>
+                <i class="fa-solid fa-chevron-right"></i>
               </button>
             </div>
-            <div class="volume--container" role="volumeContainer">
-              <button class="volume flex" ariab-describedby="muteButton" data-volume>
+            <button class="forward--btn btn flex" id="forwardButton" aria-describedby="playButton" role="button" data-skip="+25">
+              <i class="fa-solid fa-forward"></i>
+              +25
+            </button>
+            <div class="volume--container flex">
+              <button class="volume flex" data-volume>
                 <i class="fa-solid fa-volume-high"></i>
               </button>
-              <input class="slider" type="range" name="slider" value="4" min="0" max="100" step="0.5" id="slider" aria-describedby="slider" role="slider" data-slider>
+              <input class="slider" type="range" name="slider" min="0" max="100" id="slider" aria-describedby="slider" role="slider" data-slider>
             </div>
-            <div class="expand--container">
+            <div class="expand--container flex">
               <button class="expand--btn" aria-describedby="expandButton" role="switch" data-expand="expand">
                 <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
               </button>
             </div>
-          </section>
+          </div>
         </div>
       </div>
-      <figcaption class="status--container">
-        <p class="status" data-status="status" aria-describedby="status"></p>
-        <p class="artist" data-artist="artist" aria-describedby="status"></p>
+      <figcaption class="feature--container flex">
+        <p class="song--name" role="dialog" aria-describedby="status" data-song></p>
+        <div class="flex">
+          <p class="artist--name" role="dialog" data-artist></p>
+          <span class="dash"></span> 
+          <p class="album--name" role="dialog" data-album></p>
+        </div>
       </figcaption>
     </figure>
   `;
