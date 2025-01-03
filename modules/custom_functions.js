49340a2005c1f4fdae4c57d248aafc14f3f0caef
Damian Ponce <dponcez_git@hotmail.com>
2025-01-02 22:23:13 -0500

    feat: delete unnecessary comments in file (custom_functions.js)

--- modules/custom_functions.js
+++ modules/custom_functions.js
@@ -1,11 +1,4 @@
-// select a single element
 export const selector = (element) => document.querySelector(element);
-
-// select multiple elements
 export const selectorAll = (element) => document.querySelectorAll(element);
-
-// event
-export const eventHandler = ($, event, callback) =>
-  $.addEventListener(event, callback);
-
+export const eventHandler = ($, event, callback) => $.addEventListener(event, callback);
 export const log = (value) => console.log(value);
